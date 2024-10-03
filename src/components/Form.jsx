5f266d2e7a0d60ac5d628f0dd351de9791182262
Myo Zin Kyaw (ATO) <158016549+mzk-ato@users.noreply.github.com>
2024-10-03 14:07:55 +0630

    day3 form input handling and submit handling

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
 
 export default function Form() {
-  const [name, setName] = useState("");
-  function handleChange(e) {
-    setName(e.target.value);
+  const [name, setName] = useState({firstName: '', lastName: ''});
+  function handleSubmit(e) {
+    e.preventDefault();
     console.log(name);
+    setName({firstName: '', lastName: ''});
   }
   return (
     <div>
+      Full Name : {name.firstName} {name.lastName}
       <form>
-        <input type="text" onChange={(e) => handleChange(e)} value={name} />
+        <input type="text" onChange={(e) => setName({...name, firstName: e.target.value})} value={name.firstName} placeholder="First Name" />
+        <input type="text" onChange={(e) => setName({...name, lastName: e.target.value})} value={name.lastName} placeholder="Last Name" />
+        <button onClick={(e) => handleSubmit(e)}>Submit</button>
       </form>
     </div>
   );
