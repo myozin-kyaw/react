f0094f8dcd3d7620e8e8ea08393a72c1e02d8e30
Myo Zin Kyaw <mzk@ahtaroo.com>
2024-10-04 07:16:43 +0630

    day4 todo app wip

---
+++ todo/src/components/Todo.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import TodoItem from "./TodoItem";
+
+export default function Todo() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("");
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
+        <button type="submit">Submit</button>
+      </form>
+
+      {todos.map((todo) => (
+        <TodoItem key={todo} todo={todo} />
+      ))}
+    </div>
+  );
+}
---
+++ todo/src/components/TodoItem.jsx
@@ -0,0 +1,3 @@
+export default function TodoItem(props) {
+  return <h1>{props.todo}</h1>;
+}
