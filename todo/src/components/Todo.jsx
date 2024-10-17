8cf5296859a8e5fdc5d5550f463a40e655590a6a
Myo Zin Kyaw <mzk@ahtaroo.com>
2024-10-17 14:31:35 +0630

    day7 todos list sorting

--- todo/src/components/Todo.jsx
+++ todo/src/components/Todo.jsx
@@ -4,14 +4,17 @@ import TodoItem from "./TodoItem";
 
 export default function Todo() {
   const [todos, setTodos] = useState([]);
+  const sortedTodos = todos
+    .slice()
+    .sort((a, b) => Number(a.isCompleted) - Number(b.isCompleted));
   const completedTodos = todos.filter((todo) => todo.isCompleted).length;
   return (
     <div className="flex justify-center items-center">
-      <div className="max-w-md w-full sm:w-1/2 todo-container px-3 py-8">
+      <div className="max-w-md w-full sm:w-1/2 todo-container px-3 py-8 space-y-5">
         <TodoForm todos={todos} setTodos={setTodos} />
         {/* todo-list */}
         <ul className="flex flex-col space-y-2 text-gray-800">
-          {todos.map((todo) => (
+          {sortedTodos.map((todo) => (
             <TodoItem
               key={todo.id}
               todo={todo}
@@ -20,8 +23,14 @@ export default function Todo() {
             />
           ))}
         </ul>
-        Completed Todos :
-        {todos.length > 0 ? completedTodos + "/" + todos.length : ""}
+        {todos.length > 0 && (
+          <div className="bg-slate-700 text-white p-2 rounded-md shadow-md flex justify-between">
+            <div>Completed Todos : </div>
+            <div>
+              {completedTodos}/{todos.length}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
