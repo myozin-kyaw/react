273bc467d008661464b5b8d65789118c5b6dc145
Myo Zin Kyaw <myozinkyaw103@gmail.com>
2025-01-02 21:14:51 +0630

    route with programatically and route state locatoin learning

--- basic_react/src/pages/Product.jsx
+++ basic_react/src/pages/Product.jsx
@@ -1,12 +1,31 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 export default function Product() {
+  const [username, setUserName] = useState("");
   const { id } = useParams();
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/dashboard/profile", { state: { username } });
+  }
   return (
     <div>
       <h1>Product List</h1>
 
       <h3>Product ID: {id}</h3>
+
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <button
+        type="button"
+        onClick={handleClick}
+      >
+        Go to Profile Page
+      </button>
     </div>
   );
 }
--- basic_react/src/pages/Profile.jsx
+++ basic_react/src/pages/Profile.jsx
@@ -1,3 +1,7 @@
+import { useLocation } from "react-router-dom";
+
 export default function Profile() {
-  return <div>This is Profile Page.</div>;
+  const location = useLocation();
+  const { username } = location.state || {};
+  return <div>This is Profile Page for {username}</div>;
 }
