3975bbd40ff1f650ead954248e123df2e1b05a56
Myo Zin Kyaw <myozinkyaw103@gmail.com>
2025-01-06 21:18:21 +0630

    react static page in wip:

---
+++ free-code-camp-tuto/src/projects/react-static-page/ReactStaticPage.jsx
@@ -0,0 +1,12 @@
+import Main from "./components/Main";
+import NavBar from "./components/NavBar";
+import "./assets/css/ReactStaticPage.css";
+
+export default function ReactStaticPage() {
+  return (
+    <>
+      <NavBar />
+      <Main />
+    </>
+  );
+}
---
+++ free-code-camp-tuto/src/projects/react-static-page/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import ReactLogo from "./ReactLogo";
+
+export default function NavBar() {
+  return (
+    <header>
+      <nav>
+        <ReactLogo />
+      </nav>
+    </header>
+  );
+}
