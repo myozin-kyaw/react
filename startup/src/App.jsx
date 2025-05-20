2c9843301ba10684618cfc19785d5c421478db9e
Myo Zin Kyaw (ATO) <158016549+mzk-ato@users.noreply.github.com>
2025-05-20 16:40:55 +0630

    init component start-up

---
+++ startup/src/App.jsx
@@ -0,0 +1,21 @@
+import LessonCard from "./components/LessonCard";
+
+function App() {
+  return (
+    <>
+      <div className="m-5">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
+          <LessonCard />
+          <LessonCard />
+          <LessonCard />
+          <LessonCard />
+          <LessonCard />
+          <LessonCard />
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ startup/src/components/LessonCard.jsx
@@ -0,0 +1,9 @@
+function LessonCard() {
+    return (
+        <div className="p-3 bg-red-100">
+            This is Card.
+        </div>
+    );
+}
+
+export default LessonCard;
