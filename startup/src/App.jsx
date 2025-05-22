52178815eddeab95aa77d7a72235fac1a3a0ee37
Myo Zin Kyaw (ATO) <158016549+mzk-ato@users.noreply.github.com>
2025-05-22 16:04:56 +0630

    React Component (MyButton)

--- startup/src/App.jsx
+++ startup/src/App.jsx
@@ -7,12 +7,9 @@ function App() {
       <ButtonPage />
       {/* <div className="m-5">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
-          <LessonCard />
-          <LessonCard />
-          <LessonCard />
-          <LessonCard />
-          <LessonCard />
-          <LessonCard />
+          <LessonCard
+            label="Button Components"
+          />
         </div>
       </div> */}
 
--- startup/src/components/LessonCard.jsx
+++ startup/src/components/LessonCard.jsx
@@ -1,7 +1,9 @@
-function LessonCard() {
+function LessonCard({ label }) {
+    const lessonLabel = label ?? 'This is Card';
+
     return (
         <div className="p-3 bg-red-100">
-            This is Card.
+            <h1 className="font-semibold text-xl">{lessonLabel}</h1>
         </div>
     );
 }
