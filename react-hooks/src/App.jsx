f9ecda7243de8877a352a1a3445270a9f0dec825
Myo Zin Kyaw <myozinkyaw103@gmail.com>
2025-08-27 14:59:12 +0630

    Update App.jsx

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       {/* <UseStateExample/> */}
       {/* <UseEffectExample/> */}
       {/* <UseContextExample/> */}
-      <UseReducerExample/>
+      {/* <UseReducerExample/> */}
     </div>
   )
 }
